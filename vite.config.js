bf0978ea510c2d2cdb154489d1992756f5164eb0
hirono inoue <hironoinoue@hirononoMacBook-Pro.local>
2026-04-30 01:09:51 +0900

    fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
 
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        article: 'article.html',
+        updates: 'updates.html',
+      },
+    },
   },
 });
